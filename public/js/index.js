f2e40d33db51500da561d3bfdb106adb9d5ae624
Adrian J Lopez <adrianjaylopez@gmail.com>
2016-09-01 11:43:37 -0400

    Added personlist and changed the render to use personlist in public/js/index.js

--- public/js/index.js
+++ public/js/index.js
@@ -15,7 +15,18 @@ var Person = function() {
         </div>
     );
 };
+var PersonList = function() {
+    return (
+        <div className="person-list">
+            <Person />
+            <Person />
+            <Person />
+            <Person />
+            <Person />
+        </div>
+    );
+};
 
 document.addEventListener('DOMContentLoaded', function() {
-    ReactDOM.render(<Person />, document.getElementById('app'));
+    ReactDOM.render(<PersonList />, document.getElementById('app'));
 });
