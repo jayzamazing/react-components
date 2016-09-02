6c7bd364df3bb29656fcc432eee1652b86f5f3ee
Adrian J Lopez <adrianjaylopez@gmail.com>
2016-09-02 11:32:59 -0400

    Changed person to use state.

--- public/js/index.js
+++ public/js/index.js
@@ -1,20 +1,30 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var Person = function(props) {
-    return (
-        <div className="person">
-            <div className="person-name">{props.name}</div>
-            <img className="person-img" src={props.imageUrl} />
-            <div className="person-job">
-                {props.job}
+var Person = React.createClass({
+    getInitialState: function() {
+        return {
+            highlight: false
+        };
+    },
+    onClick: function() {
+        this.setState({
+            highlight: !this.state.highlight
+        });
+    },
+    render: function() {
+        var classes = 'person ' + (this.state.highlight ? 'highlight' : '');
+        return (
+            <div className={classes} onClick={this.onClick}>
+                <div className="person-name">{this.props.name}</div>
+                <img className="person-img" src={this.props.imageUrl} />
+                <div className="person-job">
+                    {this.props.job}
+                </div>
             </div>
-        </div>
-    );
-};
-Person.defaultProps = {
-    imageUrl: 'http://www.gravatar.com/avatar/?d=identicon'
-};
+        );
+    }
+});
 var PersonList = function() {
     return (
         <div className="person-list">
