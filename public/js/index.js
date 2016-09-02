3e28bb7589cb1d22653f17826886e23d3aef5eed
Adrian J Lopez <adrianjaylopez@gmail.com>
2016-09-02 10:31:17 -0400

    Changed person and personlist functions to use props.

--- public/js/index.js
+++ public/js/index.js
@@ -1,33 +1,32 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var Person = function() {
-    var name = 'Derek Zoolander';
-    var imageUrl = 'http://uifaces.com/assets/static/images/zoolander.jpg';
-    var job = 'Male model';
+var Person = function(props) {
     return (
         <div className="person">
-            <div className="person-name">{name}</div>
-            <img className="person-img" src={imageUrl} />
+            <div className="person-name">{props.name}</div>
+            <img className="person-img" src={props.imageUrl} />
             <div className="person-job">
-                {job}
+                {props.job}
             </div>
         </div>
     );
 };
-var PersonList = React.createClass({
-    render: function() {
-        var people = [];
-        for (var i=0; i<5; i++) {
-            people.push(<Person />);
-        }
-        return (
-            <div className="person-list">
-                {people}
-            </div>
-        );
-    }
-});
+Person.defaultProps = {
+    imageUrl: 'http://www.gravatar.com/avatar/?d=identicon'
+};
+var PersonList = function() {
+    return (
+        <div className="person-list">
+            <Person name="Derek Zoolander"
+                    imageUrl="http://uifaces.com/assets/static/images/zoolander.jpg"
+                    job="Male model" />
+            <Person name="Donald Knuth"
+                    imageUrl="http://www-cs-faculty.stanford.edu/~uno/don.gif"
+                    job="Clever chap" />
+        </div>
+    );
+};
 
 document.addEventListener('DOMContentLoaded', function() {
     ReactDOM.render(<PersonList />, document.getElementById('app'));
