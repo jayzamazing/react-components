87420719451d9c186ea3b98bd38ba49f52de39cb
Adrian J Lopez <adrianjaylopez@gmail.com>
2016-09-03 11:52:26 -0400

    Added soundcloudembed, button, and surprise components.

--- public/js/index.js
+++ public/js/index.js
@@ -37,7 +37,40 @@ var PersonList = function() {
         </div>
     );
 };
+var SoundCloudEmbed = function(props) {
+    var playerUrl = 'https://w.soundcloud.com/player/';
+    var trackUrl = 'https://api.soundcloud.com/tracks/' + props.trackId;
+    var options = 'auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&visual=true';
+    var src = playerUrl + '?url=' + trackUrl + '&' + options;
+    return <iframe width="100%" height="450" scrolling="no" frameborder="no" src={src}></iframe>;
+};
+
+var Button = function(props) {
+    return <button onClick={props.onClick}>{props.text}</button>;
+};
+
+var Surprise = React.createClass({
+    getInitialState: function() {
+        return {
+            clicked: false
+        };
+    },
+    onButtonClick: function() {
+        this.setState({
+            clicked: true
+        });
+    },
+    render: function() {
+        return (
+            <div>
+                <Button onClick={this.onButtonClick} text="Ready to be amazed?" />
+                {this.state.clicked ? <SoundCloudEmbed trackId="191075550" /> : null}
+            </div>
+        );
+    }
+});
 
 document.addEventListener('DOMContentLoaded', function() {
     ReactDOM.render(<PersonList />, document.getElementById('app'));
+    ReactDOM.render(<Surprise />, document.getElementById('app2'));
 });
